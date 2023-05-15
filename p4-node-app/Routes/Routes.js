1c3abdaa7911d164f0a68d92dd04510e3d521ad2
Jagger85 <jagger85.crypto@gmail.com>
2023-05-15 23:40:47 +0800

    CHORE: Document formated

--- p4-node-app/Routes/Routes.js
+++ p4-node-app/Routes/Routes.js
@@ -1,94 +1,88 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../Models/User");
-const Portfolio = require("../Models/Portfolio");
-const Wallet = require("../Models/Wallet");
+const User = require('../Models/User');
+const Portfolio = require('../Models/Portfolio');
+const Wallet = require('../Models/Wallet');
 
 //Get User
-router.get('/user/:username', (req, res)=>{
-    try{
-        User.findOne({username: req.params.username}).then(data =>{
-          res.status(200).send({user: data})
-        })
-    }catch(e){
-        throw e
-    }
-})
+router.get('/user/:username', (req, res) => {
+  try {
+    User.findOne({ username: req.params.username }).then((data) => {
+      res.status(200).send({ user: data });
+    });
+  } catch (e) {
+    throw e;
+  }
+});
 
 //Get User portfolios
-router.get('/user/:username/portfolios', (req, res)=>{
-    try{
-        console.log(req.params.username)
-        User.findOne({username: req.params.username}).then(data =>{
-            res.status(200).send({portfolios: data.portfolios})
-          })
-    }catch(e){
-        throw e
-    }
-})
+router.get('/user/:username/portfolios', (req, res) => {
+  try {
+    User.findOne({ username: req.params.username }).then((data) => {
+      res.status(200).send({ portfolios: data.portfolios });
+    });
+  } catch (e) {
+    throw e;
+  }
+});
 
 //Get User portfolio
-router.get('/user/portfolios/:username', (req, res)=>{
-    try{
-        User.find({username: req.params.username}).then(data =>{
-          res.status(200).send({user: data})
-        })
-    }catch(e){
-        throw e
-    }
-})
-
+router.get('/user/portfolios/:username', (req, res) => {
+  try {
+    User.find({ username: req.params.username }).then((data) => {
+      res.status(200).send({ user: data });
+    });
+  } catch (e) {
+    throw e;
+  }
+});
 
 //Get portfolio wallets
-router.get('/', (req, res)=>{
-    try{
-
-    }catch(e){
-        throw e
-    }
-})
+router.get('/', (req, res) => {
+  try {
+  } catch (e) {
+    throw e;
+  }
+});
 
 //Get wallet
-router.get('/', (req, res)=>{
-    try{
-
-    }catch(e){
-        throw e
-    }
-})
+router.get('/', (req, res) => {
+  try {
+  } catch (e) {
+    throw e;
+  }
+});
 //Create User
-router.post('/user/:username/', (req, res)=>{
-    //TODO check if user exist
-    try{
-        const user = new User({username : req.params.username, email: req.body.email, password: req.body.password})
-        user.save().then(data => {
-        res.status(201).send(data)
-    })
-    }catch(e){
-        throw e
-    }
-})
+router.post('/user/:username/', (req, res) => {
+  //TODO check if user exist
+  try {
+    const user = new User({ username: req.params.username, email: req.body.email, password: req.body.password });
+    user.save().then((data) => {
+      res.status(201).send(data);
+    });
+  } catch (e) {
+    throw e;
+  }
+});
 
 //Add Portfolio to user
-router.post('/user/portfolios/:username', (req, res)=>{
+router.post('/user/portfolios/:username', (req, res) => {
+  /**
+   * @TODO I should check if the user exist first I guess  🤷‍♂️
+   */
 
-    /**
-     * @TODO I should check if the user exist first I guess  🤷‍♂️
-     */
+  try {
+    const portfolio = new Portfolio({ alias: req.body.alias });
+    portfolio.save();
 
-    try{
-        const portfolio = new Portfolio({alias : req.body.alias})
-        portfolio.save()
-
-        User.findOne({username: req.params.username}).then(data => {
-           data.portfolios.push(portfolio)
-           data.save().then(data => res.send(data))
-        })
-
-    }catch(e){
-        throw e
-    }
-})
+    User.findOne({ username: req.params.username }).then((data) => {
+      data.portfolios.push(portfolio);
+      data.save().then((data) => res.send(data));
+    });
+  } catch (e) {
+    throw e;
+  }
+});
 
 //Create Wallet
 
@@ -100,5 +94,4 @@ router.post('/user/portfolios/:username', (req, res)=>{
 
 //Delete Wallet
 
-
-module.exports = router
+module.exports = router;
