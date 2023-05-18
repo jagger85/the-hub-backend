302a6cfe0f4c8e2ea0163fb6b173a7cda67d3be9
Jagger85 <jagger85.crypto@gmail.com>
2023-05-18 10:42:03 +0800

    CHORE: Code formatted

--- p4-node-app/Routes/Routes.js
+++ p4-node-app/Routes/Routes.js
@@ -27,8 +27,8 @@ router.get('/user/', (req, res) => {
 //Get User portfolios
 router.get('/user/portfolios/:username', (req, res) => {
   try {
-    User.findOne({ username: req.params.username }).then( user => {
-        res.status(200).send({portfolios : user.portfolios});
+    User.findOne({ username: req.params.username }).then((user) => {
+      res.status(200).send({ portfolios: user.portfolios });
     });
   } catch (e) {
     throw e;
@@ -37,21 +37,20 @@ router.get('/user/portfolios/:username', (req, res) => {
 
 //Get User portfolio
 router.get('/user/:username/:portfolioname', (req, res) => {
- User.findOne({ username: req.params.username }).then( user => {
-  portfolio = user.portfolios.filter( e => e.alias == req.params.portfolioname)
-  res.send({portfolio : portfolio[0]})
- })
-
+  User.findOne({ username: req.params.username }).then((user) => {
+    portfolio = user.portfolios.filter((e) => e.alias == req.params.portfolioname);
+    res.send({ portfolio: portfolio[0] });
+  });
 });
 
 //Get portfolio wallets
 router.get('/user/:username/:portfolioname/wallets', (req, res) => {
   try {
-    User.findOne({ username: req.params.username}).then( user => {
-      portfolio = user.portfolios.filter( e => e.alias == req.params.portfolioname)
-      console.log(portfolio[0].wallets)
-      res.send({wallets: portfolio[0].wallets})
-    })
+    User.findOne({ username: req.params.username }).then((user) => {
+      portfolio = user.portfolios.filter((e) => e.alias == req.params.portfolioname);
+      console.log(portfolio[0].wallets);
+      res.send({ wallets: portfolio[0].wallets });
+    });
   } catch (e) {
     throw e;
   }
@@ -60,11 +59,11 @@ router.get('/user/:username/:portfolioname/wallets', (req, res) => {
 //Get wallet
 router.get('/user/:username/:portfolioname/:walletname', (req, res) => {
   try {
-    User.findOne({ username: req.params.username}).then( user => {
-      portfolio = user.portfolios.filter( e => e.alias == req.params.portfolioname)
-      wallet = portfolio[0].wallets.filter( e => e.alias == req.params.walletname)
-      res.send({wallets: wallet[0]})
-    })
+    User.findOne({ username: req.params.username }).then((user) => {
+      portfolio = user.portfolios.filter((e) => e.alias == req.params.portfolioname);
+      wallet = portfolio[0].wallets.filter((e) => e.alias == req.params.walletname);
+      res.send({ wallets: wallet[0] });
+    });
   } catch (e) {
     throw e;
   }
@@ -72,7 +71,6 @@ router.get('/user/:username/:portfolioname/:walletname', (req, res) => {
 
 //Create User
 router.post('/user/', (req, res) => {
-  //TODO check if user exist
   try {
     const user = new User({ username: req.body.username, email: req.body.email, password: req.body.password });
     user.save().then((data) => {
@@ -85,7 +83,6 @@ router.post('/user/', (req, res) => {
 
 //Add Portfolio to user
 router.post('/user/portfolios/:username', (req, res) => {
-
   try {
     User.findOne({ username: req.params.username }).then((user) => {
       user.portfolios.push({ alias: req.body.alias });
@@ -98,14 +95,13 @@ router.post('/user/portfolios/:username', (req, res) => {
 
 //Add wallet to portfolio
 router.post('/user/portfolios/wallets/:username', (req, res) => {
-
   try {
     User.findOne({ username: req.params.username }).then((user) => {
-      portfolio = user.portfolios.filter( e => e.alias == req.body.alias)
-      portfolio[0].wallets.push({alias: req.body.walletAlias, address: req.body.walletAddress})
-      user.save().then( data =>{
-        res.send(data)
-      })
+      portfolio = user.portfolios.filter((e) => e.alias == req.body.alias);
+      portfolio[0].wallets.push({ alias: req.body.walletAlias, address: req.body.walletAddress });
+      user.save().then((data) => {
+        res.send(data);
+      });
     });
   } catch (e) {
     throw e;
@@ -115,22 +111,19 @@ router.post('/user/portfolios/wallets/:username', (req, res) => {
 //Update password
 
 //Delete User
-router.delete('/user/:username', (req, res)=>{
-  User.findOne({username: req.params.username})
-  .then( user => {
-      user.isDeleted = true
-      user.save().then(
-        data => res.status(200).send({message: `User ${req.params.username} is deleted`, data: data})
-      )
-    })
-})
+router.delete('/user/:username', (req, res) => {
+  User.findOne({ username: req.params.username }).then((user) => {
+    user.isDeleted = true;
+    user.save().then((data) => res.status(200).send({ message: `User ${req.params.username} is deleted`, data: data }));
+  });
+});
 //Delete Portfolio
 router.delete('/user/portfolios/:username', (req, res) => {
-
   try {
     User.findOne({ username: req.params.username }).then((user) => {
-      user.portfolios = user.portfolios.filter( e => e.alias != req.body.alias)
-      user.save().then( data => res.send(data))
+      user.portfolios = user.portfolios.filter((e) => e.alias != req.body.alias);
+      console.log('hello');
+      user.save().then((data) => res.send(data));
     });
   } catch (e) {
     throw e;
@@ -140,12 +133,11 @@ router.delete('/user/portfolios/:username', (req, res) => {
 //Delete Wallet
 router.delete('/user/portfolios/wallets/:username', (req, res) => {
   try {
-    User.findOne({username: req.params.username})
-    .then(user =>{
-      portfolio = user.portfolios.filter( e => e.alias == req.body.alias)
-      portfolio[0].wallets = portfolio[0].wallets.filter( e => e.alias != req.body.walletAlias)
-      user.save().then( user => res.status(200).send(user))
-    })
+    User.findOne({ username: req.params.username }).then((user) => {
+      portfolio = user.portfolios.filter((e) => e.alias == req.body.alias);
+      portfolio[0].wallets = portfolio[0].wallets.filter((e) => e.alias != req.body.walletAlias);
+      user.save().then((user) => res.status(200).send(user));
+    });
   } catch (e) {
     throw e;
   }
