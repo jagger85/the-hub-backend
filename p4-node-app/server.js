4532b400b71297a32ac3a2b3b97c9ee391e7de0e
Jagger85 <jagger85.crypto@gmail.com>
2023-05-19 12:43:11 +0800

    FEAT: Morgan added

--- p4-node-app/server.js
+++ p4-node-app/server.js
@@ -8,7 +8,9 @@ const PORT = process.env.SERVER_PORT
 const password = process.env.ATLAS_PASS
 const routes = require('./Routes/Routes');
 const helmet = require ("helmet");
+const morgan = require('morgan')
 
+server.use(morgan('tiny'))
 server.use(helmet())
 server.use( bodyParser.json() ); // This solves getting the body of the request
 server.use( cors() ); // Solves communication by other software
